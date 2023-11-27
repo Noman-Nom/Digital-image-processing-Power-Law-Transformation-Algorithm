4a092f2ad5b8e448e83431faf98c93447b788037
Noman-Nom <noman5456318@gmail.com>
2023-11-27 20:05:53 +0500

    power law calculation sucesfuly

--- script.js
+++ script.js
@@ -11,18 +11,32 @@ const getMatrixValues = (inputMatrix) => {
     for (let j = 0; j < table.rows[i].cells.length; j++) {
       const input = table.rows[i].cells[j].querySelector("input");
     //   console.log( typeof input.value);
-      console.log(input.value)   
+    //   console.log(input.value)   
 
-    values[i][j] = parseInt(input.value)
+    values[i][j] = parseFloat(input.value)
 
 }
 }
 // console.log(typeof values)
 // console.log(values)
-console.log("input values = " , values);   // it give me answer NAN wwhat is the probelm here
+console.log("input values = " , values);  
 return values
 };
 
 const calculate = () => {
-  getMatrixValues("inputMatrix");
+const inputMatrix =   getMatrixValues("inputMatrix");
+
+    console.log("input Matrix" , inputMatrix)
+
+    const resultMatrix = inputMatrix.map((row)=>(
+        row.map((value)=>{
+            console.log( `c x ${value} ^ 0.6 = ${value ** 0.6} ` )
+                value = value ** 0.6
+
+                return value
+        })
+
+    ))
+
+
 };
