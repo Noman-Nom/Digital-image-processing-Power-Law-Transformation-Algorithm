9898efaab6b60c0be3a0193b250b94b44e4f0314
Noman-Nom <noman5456318@gmail.com>
2023-11-27 19:17:37 +0500

    functionaliity

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello ")
+
+
+const getMatrixValues = (inputMatrix)=>{
+
+    const table = document.getElementById(inputMatrix)
+    // console.log(table)
+    // console.log(table.rows)
+}
+
+const calculate=()=>{
+
+    getMatrixValues('inputMatrix')
+  
+}
